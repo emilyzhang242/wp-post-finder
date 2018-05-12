083fd2181ba8fab8a643ca9701c7a869ca62a6fd
Emily Zhang <ez86@cornell.edu>
2018-05-12 15:22:14 -0400

    tutorial code added, but is definitely working

---
+++ background.js
@@ -0,0 +1,53 @@
+  chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+      console.log("The color is green.");
+    });
+
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {hostEquals: 'developer.chrome.com'},
+        })
+        ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+    });
+  });
+
+
+// chrome.tabs.onUpdated.addListener(function (tabId , info) {
+//     console.log(tabId);
+//     console.log(info);
+//     if (info.status === 'complete') {
+//       	// your code ...
+//     	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     		var activeTab = tabs[0];
+//     		chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+//   		});
+//     }
+// });
+
+// Called when the user clicks on the browser action.
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//   // Send a message to the active tab
+//   console.log("CLICK CLICK");
+//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     var activeTab = tabs[0];
+//     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+//   });
+// });
+
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//   chrome.storage.sync.get('state', function(data) {
+//     if (data.state === 'on') {
+//       chrome.storage.sync.set({state: 'off'});
+//         //do something, removing the script or whatever
+//         clearInterval(interval);
+//         alert('script ended');
+//     } else {
+//       chrome.storage.sync.set({state: 'on'});
+//         interval = setInterval(getWPcode, 100000);
+//         alert("script started");
+//     }
+//   });
+// });
--- content.js
+++ content.js
@@ -1,3 +1,119 @@
-var firstHref = $("a[href^='http']").eq(0).attr("href");
+var NUM_POSTS = 3;
+var HOUR_CUTOFF = 12;
+var NUM_VOTES_PER_HOUR = 25;
+var COMMENT_CUTOFF = 3;
+var VOTE_CUTOFF = 500; // the cutoff after which posts shouldn't be looked at 
+var postDic = {};
+var redditURL = "https://www.reddit.com";
+var username = "alannawu";
+var possiblePostURLs = {};
 
-console.log("eh no");
+function getWPcode() {
+	//refresh page
+	//location.reload();
+
+  var start = 0;
+  var divPosts = $("html").find("#siteTable");
+
+  var startPost = divPosts.find(".thing").get(0);
+  var startRank = $(startPost).find(".rank");
+
+  // there might be pinned posts at top. Ignore those.
+  for (var x=0; x < 2; x++) {
+    startPost = divPosts.find(".thing").get(x);
+    startRank = $(startPost).find(".rank");
+    if (startRank.html() != "1") {
+      start += 1;
+    }
+  }
+
+  for (var i=start; i < (NUM_POSTS+start); i++) {
+    var post = divPosts.find(".thing").get(i);
+    var numVotes = getNumVotes(post);
+    var numHours = getNumHours(post);
+    var ratio = numVotes/numHours;
+
+    //if the conditions aren't met, then don't need to check comments
+    if (ratio >= NUM_VOTES_PER_HOUR && numHours <= HOUR_CUTOFF) {
+      var url = getPostHTML(post);
+      possiblePostURLs[url] = {"rank": i};
+    }
+  }	
+
+  $.each(possiblePostURLs, function(key, value) {
+    var num = getNumComments(key);
+  });
+
+  console.log("HELPPPPPP");
+}
+
+function getNumVotes(post) {
+  var votes = $(post).find(".midcol").find(".unvoted").html();
+
+  if ($.isNumeric(votes)) {
+    return parseInt(votes);
+  } else {
+    votes = getVotesFromPage(post);
+  }
+}
+
+function getNumHours(post) {
+  var timestamp = $(post).find(".entry").find(".live-timestamp").html();
+  var hours = timestamp.replace(/\D/g, '');
+  return parseInt(hours);
+}
+
+function getVotesFromPage(post) {
+  var url = $(post).data("url");
+  console.log(url);
+}
+
+function getPostHTML(post) {
+  var url = $(post).data("url");
+  return redditURL+url;
+}
+
+function getNumComments(url) {
+  window.location = url;
+  $(".sitetable").load(function() {
+    console.log(window.document);
+  });
+
+  //console.log(win.document);
+  //console.log($(win).html());
+
+  //window.location = url;
+
+  // console.log("here's where we belong");
+  // $(win).load(function() {
+  //    console.log($("html").html());
+  // });
+
+  // chrome.runtime.onMessage.addListener(
+  // function(request, sender, sendResponse) {
+  //   if( request.message === "clicked_browser_action" ) {
+  //     console.log("WE MADE IT");
+  //     var firstHref = $("html").find(".sitetable").find(".thing").find(".md").html();
+  //     console.log(firstHref);
+  //   }
+  // });
+  // $(window).load(function(){
+  //   var numComments = $("html").find(".sitetable > .thing").length;
+  //   console.log(numComments);
+  //   return numComments;
+  // });
+  
+}
+
+//getWPcode();
+var interval = setInterval(getWPcode, 100000);
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+      console.log(firstHref);
+    }
+  }
+);
